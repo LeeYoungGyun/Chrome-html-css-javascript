7b61f293dd063a43712914a6e84e1bba9586c824
Lee YoungGyun <noxium@naver.com>
2022-06-17 00:07:49 +0900

    Add Array

---
+++ arrays.js
@@ -0,0 +1,18 @@
+const mon = "mon"
+const tue = "tue"
+const wed = "wed"
+const thu = "thu"
+const fri = "fri"
+const sat = "sat"
+const sun = "sun"
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+
+//Get Item From Array
+console.log(daysOfWeek[5]); // sat
+// Add one more day to the array
+
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[6]); // sun
