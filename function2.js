d028996495f951112f819a8cfad956c0bff14cf9
Lee YoungGyun <noxium@naver.com>
2022-06-19 23:40:53 +0900

    function

---
+++ function2.js
@@ -0,0 +1,24 @@
+const calculator = {
+
+    add: function (a, b){
+        console.log(a + b);
+    },
+    minus: function (a, b){
+        console.log(a - b);
+    },
+    devide: function (p, q) {
+        console.log(p / q);
+    },
+    square: function (m, n) {
+        console.log(m**n);
+    }
+
+};
+
+calculator.add(5, 1);
+
+calculator.minus(10, 3);
+
+calculator.devide(90, 3);
+
+calculator.square(2,3);
