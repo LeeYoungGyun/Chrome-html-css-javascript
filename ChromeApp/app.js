d3ef04f6f01de4bd8b1620c469c1b25e42abebaf
YoungGyun <noxium@naver.com>
2022-07-08 19:11:03 +0900

    ChromeApp

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -4,9 +4,11 @@ const loginButton = loginForm.querySelector("button");
 // const loginInput = loginForm.querySelector("#login-form input"); // 첫줄 지우고
 // const loginButton = loginForm.querySelector("#login-form button"); // 첫줄 지우고
 
-function btnClick() {
-    console.log("click");
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault(); // preventDefault : 브라우저가 기본 동작을 실행하지 못하게 막음
+    console.log(loginInput.value);
 }
 
-
-loginButton.addEventListener("click", btnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
