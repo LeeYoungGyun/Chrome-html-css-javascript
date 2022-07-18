4d27534a5563e1c9cfc51ec7cc854db6d264b292
Lee YoungGyun <noxium@naver.com>
2022-07-18 19:38:57 +0900

    todo

--- ChromeApp/js/todo.js
+++ ChromeApp/js/todo.js
@@ -15,12 +15,15 @@ function saveToDos() {
 function deleteTodo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
@@ -34,8 +37,12 @@ function handleTodoSubmit(event) {
     event.preventDefault()
     const newTodo = toDoInput.value;
     toDoInput.value = ""; // input칸을 비우기
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
